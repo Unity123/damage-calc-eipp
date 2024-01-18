e8753208dee2321e616755e8139c45fe95a20ddb
Red <lucasgreer2@gmail.com>
2024-01-17 19:48:04 -0700

    eippified it

--- src/js/shared_controls.js
+++ src/js/shared_controls.js
@@ -1251,7 +1251,7 @@ function getFirstValidSetOption() {
 	var sets = getSetOptions();
 	// NB: The first set is never valid, so we start searching after it.
 	for (var i = 1; i < sets.length; i++) {
-		if (sets[i].id && sets[i].id.indexOf('(Blank Set)') === -1) return sets[i];
+		if (sets[i].id) return sets[i];
 	}
 	return undefined;
 }
